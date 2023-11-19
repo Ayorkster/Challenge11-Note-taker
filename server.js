9313f7289c84504073b3f2769409e5db1429956f
mono <traineralaina@gmail.com>
2023-11-19 12:59:22 -0700

    fixed issue with id and routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const genId = require('generate-unique-id');
 const PORT = process.env.PORT || 3001;
 
 
@@ -8,13 +9,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-    );
-//Get route for Notes
+let notes = [];
+
+//Get route for Notes page
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
     );
+
+app.get('/api/notes', (req, res) => {
+        res.json(notes);
+      });
+      
+app.post('/api/notes', (req, res) => {
+        const newNote = req.body;
+        newNote.id = genId();
+        notes.push(newNote);
+        res.json(newNote);
+      
+      });
+
+
 
 app.listen(PORT, () =>
     console.log(`App Listening at http://localhost:${PORT}`));
